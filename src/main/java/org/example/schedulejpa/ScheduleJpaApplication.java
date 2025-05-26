c9ec7541beb249cd345452f445b6b88cae63cb9b
DaHyun-8509 <luce0359@gmail.com>
2025-05-26 09:38:09 +0900

    기본 틀 생성

---
+++ src/main/java/org/example/schedulejpa/ScheduleJpaApplication.java
@@ -0,0 +1,13 @@
+package org.example.schedulejpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleJpaApplication.class, args);
+    }
+
+}
