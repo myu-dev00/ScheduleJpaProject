d83c7edd22bb30efb294e35dac581f73e34decb8
myu <myu.dev00@gmail.com>
2025-05-26 14:27:54 +0900

    Add files via upload
    sql 파일 업로드

---
+++ schedule.sql
@@ -0,0 +1,1 @@
+INSERT INTO schedule_db.schedule (id, created_at, modified_at, contents, title, user_id) VALUES (1, '2025-05-26 14:11:07.497426', '2025-05-26 14:11:07.497426', '오전 10시, 강남', '면접 일정', 1);
---
+++ users.sql
@@ -0,0 +1,1 @@
+INSERT INTO schedule_db.users (id, email, password, username) VALUES (1, 'chris@example.com', '1234', 'chris');
